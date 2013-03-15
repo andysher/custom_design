f3e825b53c8edcab9d36302fec704e0b83ea7e05
andysher <sherkar.aniruddha@gmail.com>
2013-03-15 23:44:03 +0530

    6th

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -2,8 +2,8 @@ ActionMailer::Base.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
   :domain               => "gmail.com",
-  :user_name            => "aaaaa.bbbbbbb",
-  :password             => "1234567890",
+  :user_name            => "achanto.pict",
+  :password             => "facoteampict",
   :authentication       => "plain",
   :enable_starttls_auto => true
 }
